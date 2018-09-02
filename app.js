3d9c3734e60ddfd71701b90d5574a06b71ce5b95
Ramon Miklus <r.miklus@sportradar.com>
2018-09-02 16:15:02 +0200

    added cors npm module, support for localhost:3000

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('./config');
 
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const userRoutes = require('./api/routes/user');
@@ -14,18 +15,9 @@ app.use(bodyParser.urlencoded({ extended: false })); // parse urlencoded body of
 app.use(bodyParser.json()); // parse JSON body of POST requests
 
 // Handle CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-});
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 // Users route
 app.use('/users', userRoutes);
